215f6947e65dd8de46c8330683010f03b62bf4e7
kikilin1 <kikiqanlin@gmail.com>
2015-10-01 22:04:19 -0400

    only c note is playing

---
+++ main.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    var cnote=document.getElementById("c-note");
+    var dnote=document.getElementById("d-note");
+    var enote=document.getElementById("e-note");
+    var fnote=document.getElementById("f-note");
+    var gnote=document.getElementById("g-note");
+    var anote=document.getElementById("a-note");
+    var bnote=document.getElementById("b-note");
+
+    $("#c").on("click",function(){
+            cnote.currentTime=0;
+            cnote.play();
+        });
+
+
+    $("#d").on("click",function(){
+            cnote.currentTime=0;
+            cnote.play();
+        });
+    
+    $("#e").on("click",function(){
+            cnote.currentTime=0;
+            cnote.play();
+        });
+    
+    $("#f").on("click",function(){
+            cnote.currentTime=0;
+            cnote.play();
+        });
+    $("#g").on("click",function(){
+            cnote.currentTime=0;
+            cnote.play();
+        });
+    $("#a").on("click",function(){
+            cnote.currentTime=0;
+            cnote.play();
+        });
+    $("#d").on("click",function(){
+            cnote.currentTime=0;
+            cnote.play();
+        });
+});
+
+
+
+
+    
+
+
+                                   
+                                
+    
+    
+    
+    
+    
+   
