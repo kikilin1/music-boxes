543e46dab9240e7536d4a8ff8a03cdb186068a76
kikilin1 <kikiqanlin@gmail.com>
2015-10-02 21:14:04 -0400

    music11

--- main.js
+++ main.js
@@ -7,39 +7,75 @@ $(document).ready(function(){
     var anote=document.getElementById("a-note");
     var bnote=document.getElementById("b-note");
 
-    $("#c").on("click",function(){
-            cnote.currentTime=0;
-            cnote.play();
-        });
+    $("#c").on("click",playCnote);
+    function playCnote(){
+        cnote.currentTime=0;
+        cnote.play();
+    };
 
 
-    $("#d").on("click",function(){
-            cnote.currentTime=0;
-            cnote.play();
-        });
+    $("#d").on("click",playDnote);
+    function playDnote(){
+        dnote.currentTime=0;
+        dnote.play();
+    };
     
-    $("#e").on("click",function(){
-            cnote.currentTime=0;
-            cnote.play();
-        });
-    
-    $("#f").on("click",function(){
-            cnote.currentTime=0;
-            cnote.play();
-        });
-    $("#g").on("click",function(){
-            cnote.currentTime=0;
-            cnote.play();
-        });
-    $("#a").on("click",function(){
-            cnote.currentTime=0;
-            cnote.play();
-        });
-    $("#d").on("click",function(){
-            cnote.currentTime=0;
-            cnote.play();
-        });
+    $("#e").on("click",playEnote);
+    function playEnote(){
+        enote.currentTime=0;
+        enote.play();
+    };
+
+    $("#f").on("click",playFnote);
+    function playFnote(){
+        fnote.currentTime=0;
+        fnote.play();
+    };
+    $("#g").on("click",playGnote);
+    function playGnote(){
+        gnote.currentTime=0;
+        gnote.play();
+    };
+    $("#a").on("click",playAnote);
+    function playAnote(){
+        anote.currentTime=0;
+        anote.play();
+    };
+    $("#b").on("click",playBnote);
+    function playBnote(){
+        bnote.currentTime=0;
+        bnote.play();
+    };
+    $(document).keypress(function(evt){
+        //playCnote();
+        //console.log(evt.keyCode);
+        if (evt.keyCode===122){
+            playCnote();
+        }
+        if (evt.keyCode===120){
+            playDnote();
+        }
+        if (evt.keyCode===99){
+            playEnote();
+        }
+        if (evt.keyCode===118){
+            playFnote();
+        }
+        if (evt.keyCode===98){
+            playGnote();
+        }
+        if (evt.keyCode===110){
+            playAnote();
+        }
+        if (evt.keyCode===109){
+            playBnote();
+        }
+    });
+        
 });
+
+
+
 
 
 
